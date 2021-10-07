c49a7e4cc79a6d2f9002683ba1d4e08a468a5242
Ann-Kathrin Schubert <40502390@live.napier.ac.uk>
2021-10-07 10:58:04 +0100

    chapter 6 exercises

---
+++ get_and_post.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, url_for
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return "The default, 'root' route"
+
+@app.route("/display")
+def display():
+    return '<img src="'+url_for('static', filename='uploads/file.png')+'"/>'
+
+@app.route("/upload/", methods=['GET', 'POST'])
+def account():
+    if request.method == 'POST':
+        f = request.files['datafile']
+        f.save('static/uploads/file.png')
+        return "File Uploaded"
+    else:
+        page ='''
+        <html><body>
+            <form action="" method="post" name="form" enctype="multipart/form-data">
+                <input type="file" name="datafile"/>
+                <input type="submit" name="submit" id="submit"/>
+            </form>
+        </body></html>'''
+        return page, 200
+
+@app.route("/hello")
+def hello():
+    name = request.args.get('name', '')
+    if name == '':
+        return "no params supplied"
+    else:
+        return "Hello %s" % name
+
+@app.route("/add/<int:first>/<int:second>")
+def add(first, second):
+    return str(first+second)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
