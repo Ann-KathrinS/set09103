ede5beab75de4959b06e4b10486faf099eb598a7
Ann-Kathrin Schubert <40502390@live.napier.ac.uk>
2021-12-08 15:30:52 +0000

    templates, routes and db

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, url_for, flash, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -28,9 +28,6 @@ def init_db():
         db.commit()    
 
 
-@app.route('/test')
-def test():
-    return render_template('petTile.html'),200
 
 @app.route('/')
 def root():
@@ -40,10 +37,49 @@ def root():
 def page_not_found(error):
     return render_template('404.html'),404
 
-@app.route('/pet-added')
+@app.route('/pet-added', methods=['POST'])
 def petAdded():
+    if request.method == 'POST':
+        reportType = request.form['reportType']
+        petType = request.form['petType']
+        otherPetType = request.form['otherPetType']
+        petName = request.form['petName']
+        postcode = request.form['postcode']
+        if reportType == 'missing':
+            date = request.form['missingDate']
+        else:
+            date = request.form['foundDate']
+        petAge = request.form['petAge']
+        colourBlack = request.form['black']
+        colourWhite = request.form['white']
+        colourBrown = request.form['brown']
+
+
     return render_template('petAdded.html'),200
 
+@app.route('/report-missing-pet' methods=('GET', 'POST'))
+def report_missing():
+    return render_template('reportMissing.html'),200
+
+@app.route('/report-found-pet' methods=('GET', 'POST'))
+def report_found():
+    return render_template('reportFound.html'),200
+
+@app.route('/missing' methods=('GET', 'POST'))
+def showMissing():
+    return render_template('petTile.html'),200
+
+@app.route('/found' methods=('GET', 'POST'))
+def showFound():
+    return render_template('petTile.html'),200
+
+@app.route('/missing/<petId>/<petName>' methods=('GET', 'POST'))
+def missingPet():
+    return render_template('comment.html'),200
+
+@app.route('/found/<petId>/<petName>' methods=('GET', 'POST'))
+def foundPet():
+    return render_template('comment.html'),200
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
